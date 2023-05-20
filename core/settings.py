2828001998aec1aca1a6ec6c80e740b2e304c96f
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-20 00:34:22 -0600

    feat: implemented env variables

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", default='fake-django-key')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['activity-env.eba-fpm2q3nn.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
