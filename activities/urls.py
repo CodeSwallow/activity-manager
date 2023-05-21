67c6675df23996b7315a443eeeee08f12552b427
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-21 16:18:10 -0600

    feat: added profile page url

--- activities/urls.py
+++ activities/urls.py
@@ -5,5 +5,6 @@ from activities import views
 app_name = 'activities'
 
 urlpatterns = [
+    path('accounts/profile/', views.ProfileView.as_view(), name='profile'),
     path('', views.HomeView.as_view(), name='home'),
 ]
