bd23ba2ac1bfcb3257f4a44d2178766f6ef6aa26
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-20 22:11:42 -0600

    feat: setup login and signup template, urls and view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
-# Create your views here.
+from accounts.forms import SignUpForm
+
+
+class SignUpView(FormView):
+    template_name = 'registration/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super(SignUpView, self).form_valid(form)
