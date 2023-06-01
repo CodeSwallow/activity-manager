5d28b463f040540659f60e22a1106c3a65b91037
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-01 10:54:48 -0600

    feat: can create task and redirects to dashboard

--- activities/urls.py
+++ activities/urls.py
@@ -7,4 +7,5 @@ app_name = 'activities'
 urlpatterns = [
     path('accounts/profile/', views.ProfileView.as_view(), name='profile'),
     path('', views.HomeView.as_view(), name='home'),
+    path('create-task/', views.CreateTaskView.as_view(), name='create_task'),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from activities.models import Task
 
 
 class HomeView(View):
@@ -14,8 +17,16 @@ class ProfileView(View):
     template_name = 'activities/profile.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        tasks = Task.objects.filter(user=request.user)
+        return render(request, self.template_name, {'tasks': tasks})
 
 
-class CreateTaskView(CreateView):
+class CreateTaskView(LoginRequiredMixin, CreateView):
+    model = Task
     template_name = 'activities/create_task.html'
+    fields = ['title', 'description']
+    success_url = '/accounts/profile/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
