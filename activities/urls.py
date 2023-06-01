425ffb861fc8ee544f11767494b636fa556f3380
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-01 11:01:25 -0600

    feat: added task list view to url

--- activities/urls.py
+++ activities/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/profile/', views.ProfileView.as_view(), name='profile'),
     path('', views.HomeView.as_view(), name='home'),
     path('create-task/', views.CreateTaskView.as_view(), name='create_task'),
+    path('task-list/', views.TaskListView.as_view(), name='task_list'),
 ]
