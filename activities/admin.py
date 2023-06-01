502a2788984f05f5794efaf49967ed86a7a0dff6
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-01 11:25:47 -0600

    feat: added task to admin page

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from activities.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'completed', 'created']
+    list_filter = ['user', 'completed', 'created']
+    search_fields = ['title', 'description']
+    date_hierarchy = 'created'
+    ordering = ['completed', 'created']
