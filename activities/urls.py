d3de1b20829695c2f51b49895166916335da3957
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-03 08:18:19 -0600

    feat: added about template to urls

--- activities/urls.py
+++ activities/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from activities import views
 
@@ -7,6 +8,7 @@ app_name = 'activities'
 urlpatterns = [
     path('accounts/profile/', views.ProfileView.as_view(), name='profile'),
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', TemplateView.as_view(template_name='activities/about.html'), name='about'),
     path('create-task/', views.CreateTaskView.as_view(), name='create_task'),
     path('task-list/', views.TaskListView.as_view(), name='task_list'),
     path('task-detail/<int:pk>/', views.TaskDetailView.as_view(), name='task_detail'),
