7b8129ca4693ddedd483251a37b512dfb0a7acaa
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-05 22:43:27 -0600

    feat: added subtask to adming page

--- activities/admin.py
+++ activities/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 
-from activities.models import Task
+from activities.models import Task, SubTask
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'user', 'completed', 'created']
     list_filter = ['user', 'completed', 'created']
+    search_fields = ['title', 'description']
+    date_hierarchy = 'created'
+    ordering = ['completed', 'created']
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'task', 'completed', 'created']
+    list_filter = ['task', 'completed', 'created']
     search_fields = ['title', 'description']
     date_hierarchy = 'created'
     ordering = ['completed', 'created']
