d20b1aa21f65dcf102ac1c8c2f8b9b3ef90f2213
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-09 00:01:27 -0600

    test: added ProfileView tests

--- activities/tests/test_views.py
+++ activities/tests/test_views.py
@@ -15,6 +15,7 @@ class HomeViewTests(TestCase):
     """
     Test the HomeView view.
     """
+
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -27,3 +28,34 @@ class HomeViewTests(TestCase):
         response = self.client.get(reverse('activities:home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'activities/home.html')
+
+
+class ProfileViewTests(TestCase):
+    """
+    Test the ProfileView view.
+    """
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/accounts/profile/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('activities:profile'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('activities:profile'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('activities:profile'))
+        self.assertRedirects(response, '/accounts/login/?next=/accounts/profile/')
+
+    def test_logged_in_uses_correct_template(self):
+        user = create_user()
+        self.client.force_login(user)
+        response = self.client.get(reverse('activities:profile'))
+
+        self.assertEqual(str(response.context['user']), 'user')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'activities/profile.html')
