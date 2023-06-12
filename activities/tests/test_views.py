daec5501abbff7b2f5e5c4668920bdd37a7d130d
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-12 16:01:57 -0600

    test: added more tests for TaskListView

--- activities/tests/test_views.py
+++ activities/tests/test_views.py
@@ -11,6 +11,13 @@ def create_user(username='user', password='test_pass123'):
     return get_user_model().objects.create_user(username, password)
 
 
+def create_task(title='Task', description='Task description', created_by=None, team=None):
+    """Create and return a new task."""
+    if created_by is None:
+        created_by = create_user()
+    return Task.objects.create(title=title, description=description, created_by=created_by, team=team)
+
+
 class HomeViewTests(TestCase):
     """
     Test the HomeView view.
@@ -79,9 +86,9 @@ class ProfileViewTests(TestCase):
         self.assertEqual(response.context['tasks'].count(), 0)
 
 
-class TaskViewsTests(TestCase):
+class TaskListViewTests(TestCase):
     """
-    Test the TaskListView view and TaskDetailView view.
+    Test the TaskListView view.
     """
 
     def test_task_list_view_url_exists_at_desired_location(self):
@@ -92,6 +99,36 @@ class TaskViewsTests(TestCase):
         response = self.client.get(reverse('activities:task_list'))
         self.assertEqual(response.status_code, 302)
 
-    def test_task_list_view_uses_correct_template(self):
+    def test_redirect_if_not_logged_in(self):
         response = self.client.get(reverse('activities:task_list'))
-        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/task-list/')
+
+    def test_logged_in_uses_correct_template(self):
+        user = create_user()
+        self.client.force_login(user)
+        response = self.client.get(reverse('activities:task_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'activities/task_list.html')
+
+    def test_logged_in_uses_correct_task_context(self):
+        user = create_user()
+        self.client.force_login(user)
+        response = self.client.get(reverse('activities:task_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'activities/task_list.html')
+        self.assertEqual(response.context['tasks'].count(), 0)
+
+    def test_logged_in_user_sees_only_his_tasks(self):
+        other_user = create_user(username='other_user')
+        create_task(title='Task', description='Task description', created_by=other_user)
+
+        user = create_user()
+        create_task(title='Task', description='Task description', created_by=user)
+        self.client.force_login(user)
+        response = self.client.get(reverse('activities:task_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'activities/task_list.html')
+        self.assertEqual(response.context['tasks'].count(), 1)
