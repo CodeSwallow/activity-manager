7eb91f28f51f52879d6e05409763033fb08a4331
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-06-13 17:53:42 -0600

    feat: reversed task query order by created

--- activities/models.py
+++ activities/models.py
@@ -22,6 +22,9 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['-created']
+
 
 class SubTask(models.Model):
     title = models.CharField(max_length=200)
